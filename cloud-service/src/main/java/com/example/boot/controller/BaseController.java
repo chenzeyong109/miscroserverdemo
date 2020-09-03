d89cfde39f4e1904e5adca61ee7609222600dde6
abcwangjingjing <1360951275@qq.com>
2020-09-03 14:52:56 +0800

    --git springcloud-config-client

--- cloud-service/src/main/java/com/example/boot/controller/BaseController.java
+++ cloud-service/src/main/java/com/example/boot/controller/BaseController.java
@@ -1,5 +1,6 @@
 package com.example.boot.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,10 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/base")
 public class BaseController {
+    @Value("${env}")
+    private String env;
 
     @GetMapping(value = "/index")
     public String base() {
         System.out.println("base -1 ");
-        return "base1";
+        return "cloud-env:" + env;
     }
 }
